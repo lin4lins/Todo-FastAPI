1df92166e5564c4e81259ad71c6862779a3f10eb
elina <elina.vl.shramko@gmail.com>
2022-08-02 12:49:25 +0300

    Delete unused imports

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-from fastapi import Depends, FastAPI
+from fastapi import FastAPI
 
-from Company import companyapis, dependencies
 from Database.db_properties import Base, engine
 from Routers import auth, todos, users
 
 app = FastAPI()
 app.include_router(auth.router)
 app.include_router(todos.router)
-app.include_router(companyapis.router,
-                   prefix="/companyapis",
-                   tags=["companyapis"],
-                   dependencies=[Depends(dependencies.get_token_header)],
-                   responses={418: {"description": "Internal Use Only"}})
 app.include_router(users.router)
 
 Base.metadata.create_all(bind=engine)
