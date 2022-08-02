09d6899a862c4f143fb147da34c26fa921b94509
elina <elina.vl.shramko@gmail.com>
2022-08-02 11:26:48 +0300

    Add main configurations

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+from dotenv import load_dotenv
+from fastapi import Depends, FastAPI
 
+from Company import companyapis, dependencies
+from Database.db_properties import Base, engine
+from Routers import auth, todos, users
+
+load_dotenv()
+
+app = FastAPI()
+app.include_router(auth.router)
+app.include_router(todos.router)
+app.include_router(companyapis.router,
+                   prefix="/companyapis",
+                   tags=["companyapis"],
+                   dependencies=[Depends(dependencies.get_token_header)],
+                   responses={418: {"description": "Internal Use Only"}})
+app.include_router(users.router)
+
+Base.metadata.create_all(bind=engine)
