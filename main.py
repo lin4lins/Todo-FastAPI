dfb517b3261ce343c3df95f5f811b264e4dcec36
elina <elina.vl.shramko@gmail.com>
2022-08-02 12:57:08 +0300

    Apply css

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ from fastapi import FastAPI
 
 from Database.db_properties import Base, engine
 from Routers import auth, todos, users
+from starlette.staticfiles import StaticFiles
+
 
 app = FastAPI()
 app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(users.router)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 Base.metadata.create_all(bind=engine)
