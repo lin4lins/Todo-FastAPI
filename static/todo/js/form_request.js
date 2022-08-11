ecf49f723c8e6504dfceb1e407d61bd26477facd
elina <elina.vl.shramko@gmail.com>
2022-08-11 13:36:44 +0300

    Delete urls' protocol & hostname

--- static/todo/js/form_request.js
+++ static/todo/js/form_request.js
@@ -8,7 +8,6 @@ function send_http_request(method, url, data = null) {
     xhr.onreadystatechange = function () {
       if (xhr.readyState === 4) {
         var response = xhr.responseText;
-        console.log(response);
         if (response == "null") {
             return;
         };
@@ -41,7 +40,7 @@ function get_todo_id_str_from_url() {
 
 function add_todo() {
     var method = "POST";
-    var url = "http://127.0.0.1:8000/todos/add";
+    var url = "/todos/add";
 
     var todo_data = {
       "title": document.getElementById("title").value,
@@ -54,7 +53,7 @@ function add_todo() {
 function edit_todo() {
     var method = "PUT";
     var todo_id_str = get_todo_id_str_from_url();
-    var url = "http://127.0.0.1:8000/todos/edit"+"/"+todo_id_str;
+    var url = "/todos/edit"+"/"+todo_id_str;
 
     var todo_updated_data = {
       "title": document.getElementById("title").value,
@@ -67,13 +66,13 @@ function edit_todo() {
 function delete_todo() {
     var method = "DELETE";
     var todo_id_str = get_todo_id_str_from_url();
-    var url = "http://127.0.0.1:8000/todos/delete"+"/"+todo_id_str;
+    var url = "/todos/delete"+"/"+todo_id_str;
     send_http_request(method, url);
 }
 
 function set_todo_complete(todo_id) {
     var method = "PUT";
-    var url = "http://127.0.0.1:8000/todos/complete"+"/"+todo_id;
+    var url = "/todos/complete"+"/"+todo_id;
 
     var todo_status = {
         "completed": true
