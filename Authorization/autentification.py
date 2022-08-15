929eba6fee8732f0f7b60223c54fb234d43cc8ca
elina <elina.vl.shramko@gmail.com>
2022-08-15 21:02:44 +0300

    Optimize get_authenticated_user function with raising custom exceptions

--- Authorization/autentification.py
+++ Authorization/autentification.py
@@ -2,20 +2,23 @@ from typing import Union
 
 from Database.db_init import DatabaseUser
 from Database.db_manager import get_user_by_username
-from Exceptions import get_user_not_found_exception
 from sqlalchemy.orm import Session
 
 from Authorization.password_crypt import verify_password
+from Exceptions.TokenExceptions import IncorrectUsernameOrPasswordException
+from Exceptions.UserExceptions import UserNotFoundException
 
 
 def get_authenticated_user(username: str, password: str, session: Session) \
                                             -> Union[DatabaseUser, None]:
     user = get_user_by_username(username, session)
     if not user:
-        raise get_user_not_found_exception()
+        raise UserNotFoundException(username)
 
-    if authenticate_user(user, password):
-        return user
+    if not authenticate_user(user, password):
+        raise IncorrectUsernameOrPasswordException()
+
+    return user
 
 
 def authenticate_user(user: DatabaseUser, password_to_check: str) -> bool:
