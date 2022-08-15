d1d796ed267d31b4070236f8002c29c393af234c
elina <elina.vl.shramko@gmail.com>
2022-08-15 21:06:56 +0300

    Upgrade read_todos function with try-except

--- Routers/todos.py
+++ Routers/todos.py
@@ -1,12 +1,16 @@
 from Authorization.token_manager import get_current_user
 from Database.db_manager import (create_todo, remove_todo,
-                                 get_all_todos, get_todo_by_id_and_user_id,
-                                 update_todo, update_todo_status)
+                                 get_todo_by_id_and_user_id,
+                                 update_todo, update_todo_status,
+                                 get_all_user_todos_by_user_id)
 from Database.db_properties import get_session
 from fastapi import APIRouter, Depends, Request
 from fastapi.params import Path
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+
+from Exceptions import RootException
+from Exceptions.TokenExceptions import InvalidTokenException
 from Models.Todo import RawTodo
 from sqlalchemy.orm import Session
 from starlette.responses import JSONResponse
@@ -20,11 +24,21 @@ templates = Jinja2Templates(directory="templates")
 
 
 @router.get("/read", response_class=HTMLResponse)
-async def read_todos(request: Request, user: CurrentUser = Depends(get_current_user),
-                     session: Session = Depends(get_session)):
-    todos = get_all_todos(user.id, session)
-    return templates.TemplateResponse(name="home.html",
-                                      context={"request": request, "todos": todos})
+async def read_todos(request: Request, session: Session = Depends(get_session)):
+    try:
+        user = await get_current_user(request)
+        todos = get_all_user_todos_by_user_id(user.id, session)
+        page = "home.html"
+        context = {"request": request, "todos": todos, 'user': True}
+
+    except InvalidTokenException:
+        page = "error.html"
+        context = {"request": request, 'error': 'You are unauthorized'}
+
+    except Exception:
+        page = "error.html"
+        context = {"request": request, 'error': 'Something went wrong'}
+    return templates.TemplateResponse(name=page, context=context)
 
 
 @router.get("/add", response_class=HTMLResponse)
