4388e3c49b8bb4d37b55008ceca0edbbaff899b4
elina <elina.vl.shramko@gmail.com>
2022-08-17 12:15:37 +0300

    Create user_manager.js file to unite all functions connected with user authorization/registration

---
+++ static/todo/js/user_manager.js
@@ -0,0 +1,32 @@
+function login() {
+    var method = "POST";
+    var url = "/";
+    var login_data = {
+    "username": document.getElementById("username").value,
+    "password": document.getElementById("password").value
+    };
+
+    send_http_request(method, url, login_data);
+}
+
+function logout() {
+    var method = "POST";
+    var url = "/logout";
+    send_http_request(method, url);
+}
+
+function register() {
+    var method = "POST";
+    var url = "/signin";
+    var signin_data = {
+    "email": document.getElementById("email").value,
+    "username": document.getElementById("username").value,
+    "first_name": document.getElementById("first-name").value,
+    "last_name": document.getElementById("last-name").value,
+    "password": document.getElementById("password").value,
+    "password2": document.getElementById("password2").value
+    };
+
+    send_http_request(method, url, signin_data);
+}
+}
