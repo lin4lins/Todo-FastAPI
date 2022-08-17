d82a8c16ee4192e6a9c4921c2e859a882d3d3d33
elina <elina.vl.shramko@gmail.com>
2022-08-17 14:51:28 +0300

    Add check_current_password_match to check if input password matches real password

--- Authorization/password_crypt.py
+++ Authorization/password_crypt.py
@@ -1,6 +1,11 @@
 from passlib.context import CryptContext
+from sqlalchemy.orm import Session
 
-from Exceptions.UserExceptions import PasswordNotMatchException
+from Database.db_init import DatabaseUser
+from Database.db_manager import get_user_by_id_and_username
+from Exceptions.UserExceptions import PasswordNotMatchException, \
+    IncorrectPasswordException
+from Models.User import CurrentUser
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -16,3 +21,10 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 async def check_password_match(password1: str, password2: str) -> None:
     if not password1 == password2:
         raise PasswordNotMatchException()
+
+
+async def check_current_password_match(user: CurrentUser, input_current_password: str,
+                                       session: Session) -> None:
+    user: DatabaseUser = get_user_by_id_and_username(user.id, user.username, session)
+    if not verify_password(input_current_password, user.hashed_password):
+        raise IncorrectPasswordException()
