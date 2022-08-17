4ee382c170ac01f2f8577909541a40c00b877f94
elina <elina.vl.shramko@gmail.com>
2022-08-17 14:49:11 +0300

    Register new router

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ load_dotenv()
 from fastapi import FastAPI
 
 from Database.db_properties import Base, engine
-from Routers import auth, todos
+from Routers import auth, todos, user
 from starlette.staticfiles import StaticFiles
 
 
 app = FastAPI()
 app.include_router(auth.router)
 app.include_router(todos.router)
+app.include_router(user.router)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
