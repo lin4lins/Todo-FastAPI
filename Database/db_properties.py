6b1b862faa30d13b72b0e3ced6b99d82982cec68
elina <elina.vl.shramko@gmail.com>
2022-08-18 13:20:08 +0300

    Set PostgreSQL

--- Database/db_properties.py
+++ Database/db_properties.py
@@ -4,11 +4,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Session, sessionmaker
 
-# engine = create_engine(os.environ.get("SQLALCHEMY_DATABASE_URL"))
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
+if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.\
+        replace("postgres://", "postgresql://", 1)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
